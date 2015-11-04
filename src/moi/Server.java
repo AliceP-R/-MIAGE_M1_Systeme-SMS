7efd2f433fcd861cbafa5ee4bce79d3152caf40b
AliceP-R <alice.pottier@free.fr>
2015-11-04 11:53:02 +0100

    Commentaire

--- src/moi/Server.java
+++ src/moi/Server.java
@@ -11,11 +11,13 @@ import java.util.Hashtable;
 
 public class Server
 {
-    private Hashtable<Client, Socket> l_client = new Hashtable<>();
-    
+    private Hashtable<String, Socket> l_client = new Hashtable<>();
+    private int maxConnexion = 10;
+
     public static void main(String[] args) throws IOException {
         final int port = 483;
         System.out.println("Serveur en attente de connexion sur le port "+port);
+        // Création de la socket d'écoute
         ServerSocket ss = new ServerSocket(port);
         Socket clientSocket = ss.accept();
         System.out.println("Recieved connection from "+clientSocket.getInetAddress()+" on port "+clientSocket.getPort());
